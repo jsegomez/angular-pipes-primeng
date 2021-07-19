8121220621714b5e1a9fc760540e152ebc9bb4ca
Josué Eliezer Gómez Soto <jsegomez06@gmail.com>
2021-07-19 00:12:32 -0600

    Base para desarrollo de pipes con template de PrimeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
+  
   title = 'pipes';
+  isLoading: boolean = false;
+
+  changeState(){    
+    this.isLoading = true;
+    
+    setTimeout(() => {
+      this.isLoading = false;      
+    }, 5500);
+  }
+
+  
+
 }
