63383663755b87bb224aa4cc51df7c743aa89483
Josué Eliezer Gómez Soto <jsegomez06@gmail.com>
2021-07-26 13:38:35 -0600

    Completado hasta la clase 149

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -16,12 +16,37 @@ export class NoComunesComponent implements OnInit {
     this.primengConfig.ripple = true;
   }
 
-  name: string = 'Fernanda';
-  genre: string = 'femenino';
+  isFemenino: boolean = true;
+  name: string = this.isFemenino ? 'Karla' : 'Josué';
+  genre: string  = this.isFemenino ? 'femenino' : 'masculino';
 
   invitacionMapa = {
     'masculino' : 'invitarlo',
     'femenino'  : 'invitarla',
+  }
+
+  clientes: string[] = ['María', 'José', 'Karla', 'Josué', 'Saúl', 'Victor'];
+
+  clientesMapa = {
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': 'un ciente esperando',
+    '=2': 'dos clientes esperando',
+    'other': '# clientes esperando.' 
+  }
+
+  cambiarPersona(){
+    this.isFemenino = !this.isFemenino;
+    this.genre = this.isFemenino ? 'femenino' : 'masculino';
+    this.name = this.isFemenino ? 'Karla' : 'Josué';
+  }
+
+  eliminarPersona(){
+    const position = this.clientes.length;
+    
+    if(position == 0) return;
+    this.clientes = this.clientes.slice(1, position);
+    console.log(this.clientes.length);
+    
   }
 
 }
